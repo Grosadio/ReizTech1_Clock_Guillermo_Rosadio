cbfccf3c0e03a6697420a005f6ed1ab3a45a4c2a
Grosadio <guillermorosadiov@gmail.com>
2023-01-22 17:03:05 +0200

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Angle
+    {
+        static void Main(string[] args)
+        {   //Ask the user and store
+            int hour, min;
+            Console.WriteLine("Enter Hour:");
+            hour=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Minute:");
+            min=Convert.ToInt32(Console.ReadLine());
+            //The logic of the clock is that every hour is 30 degrees and every minute is 6 degrees
+            double hrDeg=(hour*30)+(min*30/60);
+            double minDeg = min * 6;
+
+            //A condition when the diff is greater than 180 
+            double diff=Math.Abs(hrDeg-minDeg);
+            if (diff > 180)
+                diff = 360 - diff;
+            Console.WriteLine("The angle between minute and hour is:{0}",diff);
+            Console.ReadLine();
+        }
+    }
+}
